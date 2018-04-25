41d69cdcd6e6e996e715c31bc8810f4e2681e795
Christopher Moffett <christophmoffett@gmail.com>
2018-04-25 16:46:05 -0700

    A5 update

--- driver.py
+++ driver.py
@@ -4,23 +4,49 @@ Created on Apr 15, 2018
 @author: mroch
 '''
 
-from ml_lib.example_reader import CSVDataSet
 from ml_lib.learning import (DataSet, 
                              DecisionTreeLearner, NeuralNetLearner)
 from std_cv import cross_validation
-from random import shuffle
-
+import random
 from copy import deepcopy
     
 def learn(dataset):
-
-    raise NotImplemented
-            
-
     
+    random.shuffle(dataset.examples)
+
+    dataList = cross_validation(DecisionTreeLearner, dataset)
+    print("")
+    print("Decision Tree: " + dataset.name)
+    print("Mean: %.3f" % (dataList[0]))
+    print("StdDv: %.3f" % dataList[1]) 
+    formattedList = ['%.3f' % e for e in dataList[2]]
+    print("Fold Errors: ", formattedList)  
     
+    if (dataset.name != "orings"):
+        dataset.attributes_to_numbers()
+    dataList = cross_validation(NeuralNetLearner, dataset)
+    print("")
+    print("Neural Net: " + dataset.name)
+    print("Mean: %.3f" % (dataList[0]))
+    print("StdDv: %.3f" % dataList[1]) 
+    formattedList = ['%.3f' % e for e in dataList[2]]
+    print("Fold Errors: ", formattedList)  
+
 def main():
-    raise NotImplemented
+    dset = DataSet(name = "iris")
+    learn(dset)
+    
+    dset = DataSet(name = "orings")
+    #learn(dset)
+    
+    dset = DataSet(name = "restaurant")
+    learn(dset)
+    
+    dset = DataSet(name = "zoo")
+    learn(dset)
+    
+    dset = DataSet(name = "abalone")
+    learn(dset)
     
 if __name__ == '__main__':
     main()
--- std_cv.py
+++ std_cv.py
@@ -1,7 +1,7 @@
 
 from statistics import (stdev, mean)
 from ml_lib.learning import (err_ratio, train_and_test)
-
+import random
 
 def cross_validation(learner, dataset, k=10):
     """Perform k-fold cross_validation
@@ -10,5 +10,46 @@ def cross_validation(learner, dataset, k=10):
     
     Returns tuple (mean_err, std_err, fold_errors, models)
     """
+    
+    """Do k-fold cross_validate and return their mean.
+    That is, keep out 1/k of the examples for testing on each of k runs.
+    Shuffle the examples first; if trials>1, average over several shuffles.
+    Returns Training error, Validation error"""
+    
+#     if k is None:
+#         k = len(dataset.examples)
 
-    raise NotImplemented
+    foldData = []
+    fold_errT = 0   # fold error on training data
+    fold_errV = 0   # fold error on validation data
+    
+    n = len(dataset.examples)
+    examples = dataset.examples
+    
+    for fold in range(k):   # for each fold
+        # Shuffle all data            
+        #random.shuffle(dataset.examples)
+        
+        # Split into train and test
+        # Note that this is not a canonical cross validation where
+        # every pieces of data is used for training and testing
+        # due to the shuffling above.
+        train_data, val_data = train_and_test(dataset, fold * (n / k),
+                                              (fold + 1) * (n / k))
+        
+        dataset.examples = train_data
+        h = learner(dataset)
+        
+        # predict and accumulate the error rate on 
+        # the training and validation data
+        fold_errT += err_ratio(h, dataset, train_data)
+        fold_errV += err_ratio(h, dataset, val_data)
+        
+        foldData.append(err_ratio(h, dataset, val_data)) #validation error
+        
+        # Reverting back to original once test is completed
+        dataset.examples = examples
+        
+    # Return average per fold rates
+    return mean(foldData), stdev(foldData), foldData
+
